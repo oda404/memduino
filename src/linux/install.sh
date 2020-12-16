8c01bb5554f08d0cf5316f175146105de7d0cd61
oda404 <xdxalexandru404@gmail.com>
2020-12-16 15:34:48 +0200

    Tweak install script

--- src/linux/install.sh
+++ src/linux/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 #this script should only be executed through make install
+sudo systemctl stop memduinod
 sudo cp memDuino /usr/bin/
 sudo cp $(dirname $BASH_SOURCE)/memduinod.service /usr/lib/systemd/system/
 sudo chmod 644 /usr/lib/systemd/system/memduinod.service
 sudo systemctl enable memduinod
+sudo systemctl start memduinod
