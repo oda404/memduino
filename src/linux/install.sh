5b54ff633f67533b42d22012e4ed31f5cf5dadeb
oda404 <xdxalexandru404@gmail.com>
2021-02-10 23:53:50 +0200

    Rename binary from memDuino to memduino

--- src/linux/install.sh
+++ src/linux/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #this script should only be executed through make install
 sudo systemctl stop memduinod
-sudo cp memDuino /usr/bin/
+sudo cp memduino /usr/bin/
 sudo cp $(dirname $BASH_SOURCE)/memduinod.service /usr/lib/systemd/system/
 sudo chmod 644 /usr/lib/systemd/system/memduinod.service
 sudo systemctl enable memduinod
