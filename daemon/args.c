fc4c2716121a3e0a680690c6f45d3e781802accb
oda404 <xdxalexandru404@gmail.com>
2021-02-17 16:32:45 +0200

    Rework arg parser

--- daemon/args.c
+++ daemon/args.c
@@ -3,23 +3,14 @@
 
 #include"args.h"
 
-#include<string.h>
 #include<stdio.h>
-#include<ctype.h>
+#include<string.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
-#define HELP_ARG_SHORT            "-h"
-#define HELP_ARG_LONG             "--help"
-#define UPDATE_INTERVAL_ARG_SHORT "-u"
-#define UPDATE_INTERVAL_ARG_LONG  "--update-interval"
-#define INIT_TIMEOUT_ARG_SHORT    "-i"
-#define INIT_TIMEOUT_ARG_LONG     "--init-timeout"
-#define INIT_TRY_SLEEP_ARG_SHORT  "-I"
-#define INIT_TRY_SLEEP_ARG_LONG   "--init-try-sleep"
-#define SERIAL_PORT_ARG_SHORT     "-s"
-#define SERIAL_PORT_ARG_LONG      "--serial-port"
+#define UI unsigned int
 
-static void print_help_message()
+void print_help_message()
 {
 printf("\
 Usage: \n\
@@ -37,189 +28,144 @@ Options:\n\
 ");
 }
 
-static int get_arg_index(
-    int argc,
-    char **argv,
-    char *arg_short,
-    char *arg_long
+void args_init(Args *args)
+{
+    args->args_arr = NULL;
+    args->argsc = 0;
+}
+
+void args_add(
+    const char *name,
+    const char *arg_short,
+    const char *arg_long,
+    bool is_flag,
+    Args *args
 )
 {
+    /* make space for the new arg */
+    args->args_arr = 
+    realloc(
+        args->args_arr, 
+        sizeof(Arg) * (args->argsc + 1)
+    );
+
+    Arg *head_arg = &args->args_arr[args->argsc];
+
+    head_arg->name = malloc(sizeof(char) * (strlen(name) + 1));
+    strcpy(head_arg->name, name);
+    head_arg->arg_short = malloc(sizeof(char) * (strlen(arg_short) + 1));
+    strcpy(head_arg->arg_short, arg_short);
+    head_arg->arg_long = malloc(sizeof(char) * (strlen(arg_long) + 1));
+    strcpy(head_arg->arg_long, arg_long);
+    head_arg->is_flag = is_flag;
+    head_arg->value = NULL;
+
+    ++args->argsc;
+}
+
+void args_free(Args *args)
+{
     UI i = 0;
-    for(; i < (UI)argc; ++i)
+    for(; i < args->argsc; ++i)
     {
-        if(
-            strcmp(argv[i], arg_short) == 0 || 
-            strcmp(argv[i], arg_long) == 0
-        )
-        {
-            return i;
-        }
+        free(args->args_arr[i].value);
+        free(args->args_arr[i].name);
+        free(args->args_arr[i].arg_short);
+        free(args->args_arr[i].arg_long);
     }
-
-    return -1;
+    free(args->args_arr);
+    /* clean up */
+    args_init(args);
 }
 
-static int str_is_unsigned_number(const char *str)
+void args_parse(int argc, char **argv, Args *args)
+{
+    UI argv_i = 0;
+    UI args_i = 0;
+
+    for(; args_i < args->argsc; ++args_i)
+    {
+        for(argv_i = 0; argv_i < (UI)argc; ++argv_i)
+        {
+            Arg *arg_tmp = &args->args_arr[args_i];
+            if(
+                strcmp(arg_tmp->arg_short, argv[argv_i]) == 0 ||
+                strcmp(arg_tmp->arg_long, argv[argv_i]) == 0
+            )
+            {
+                if(arg_tmp->is_flag)
+                {
+                    arg_tmp->value = malloc(sizeof(bool));
+                    *(bool *)arg_tmp->value = true;
+                }
+                else if(argv_i + 1 < (UI)argc)
+                {
+                    arg_tmp->value =
+                    malloc(sizeof(char) * (strlen(argv[argv_i + 1]) + 1));
+                    strcpy(arg_tmp->value, argv[argv_i + 1]);
+                }
+                else
+                {
+                    printf("[argparser] Warning: Invalid value for arg '%s'\n", argv[argv_i]);
+                }
+            }
+        }
+    }
+}
+
+bool args_get_bool(const char *name, Args *args)
 {
     UI i = 0;
-    for(; i < strlen(str); ++i)
+    for(; i < args->argsc; i++)
     {
-        if(!isdigit(str[i])) return 0;
+        Arg *tmp_arg = &args->args_arr[i];
+        if(strcmp(tmp_arg->name, name) == 0)
+        {
+            return tmp_arg->value ? true : false;
+        }
     }
 
-    return 1;
+    return false;
 }
 
-static void print_invalid_arg_error(char *arg)
+int args_out_str(const char *name, char **out, Args *args)
 {
-    printf("Fatal error: Can't parse '%s' arg because it's value is invalid", arg);
-}
-
-UI check_for_help_arg(int argc, char **argv)
-{
-    int arg_index = get_arg_index(
-        argc,
-        argv,
-        HELP_ARG_SHORT,
-        HELP_ARG_LONG
-    );
-
-    if(arg_index > -1)
+    UI i = 0;
+    for(; i < args->argsc; ++i)
     {
-        print_help_message();
-        return ARG_OK;
+        Arg *tmp_arg = &args->args_arr[i];
+        if(strcmp(tmp_arg->name, name) == 0)
+        {
+            if(tmp_arg->value)
+            {
+                free(*out);
+                *out = malloc(sizeof(char) * (strlen(tmp_arg->value) + 1));
+                strcpy(*out, tmp_arg->value);
+                return ARG_OK;
+            }
+            return ARG_BAD;
+        }
     }
 
-    return ARG_NOT_FOUND;
+    return ARG_OK;
 }
 
-UI get_update_interval_arg(int argc, char **argv, UI *out)
+int args_out_uint(const char *name, UI *out, Args *args)
 {
-    int arg_index = get_arg_index(
-        argc,
-        argv,
-        UPDATE_INTERVAL_ARG_SHORT,
-        UPDATE_INTERVAL_ARG_LONG
-    );
-
-    if(arg_index > -1)
+    UI i = 0;
+    for(; i < args->argsc; ++i)
     {
-        /* if next arg can't be accessed return bad */
-        if(arg_index + 1 > argc - 1)
+        Arg *tmp_arg = &args->args_arr[i];
+        if(strcmp(tmp_arg->name, name) == 0)
         {
-            print_invalid_arg_error(argv[arg_index]);
+            if(tmp_arg->value)
+            {
+                *out = (UI)atoi(tmp_arg->value);
+                return ARG_OK;
+            }
             return ARG_BAD;
         }
-
-        const char *tmp = argv[arg_index + 1];
-        
-        /* if next arg isn't a number return bad */
-        if(!str_is_unsigned_number(tmp))
-        {
-            print_invalid_arg_error(argv[arg_index]);
-            return ARG_BAD;
-        }
-
-        *out = atoi(tmp);
-        return ARG_OK;
     }
 
-    return ARG_NOT_FOUND;
-}
-
-UI get_init_timeout_arg(int argc, char **argv, UI *out)
-{
-    int arg_index = get_arg_index(
-        argc,
-        argv,
-        INIT_TIMEOUT_ARG_SHORT,
-        INIT_TIMEOUT_ARG_LONG
-    );
-
-    if(arg_index > -1)
-    {
-        /* if next arg can't be accessed return bad */
-        if(arg_index + 1 > argc - 1)
-        {
-            print_invalid_arg_error(argv[arg_index]);
-            return ARG_BAD;
-        }
-
-        const char *tmp = argv[arg_index + 1];
-        
-        /* if next arg isn't a number return bad */
-        if(!str_is_unsigned_number(tmp))
-        {
-            print_invalid_arg_error(argv[arg_index]);
-            return ARG_BAD;
-        }
-
-        *out = atoi(tmp);        
-        return ARG_OK;
-    }
-
-    return ARG_NOT_FOUND;
-}
-
-UI get_init_try_sleep_arg(int argc, char **argv, UI *out)
-{
-    int arg_index = get_arg_index(
-        argc,
-        argv,
-        INIT_TRY_SLEEP_ARG_SHORT,
-        INIT_TRY_SLEEP_ARG_LONG
-    );
-
-    if(arg_index > -1)
-    {
-        /* if next arg can't be accessed return bad */
-        if(arg_index + 1 > argc - 1)
-        {
-            print_invalid_arg_error(argv[arg_index]);
-            return ARG_BAD;
-        }
-
-        const char *tmp = argv[arg_index + 1];
-        
-        /* if next arg isn't a number return bad */
-        if(!str_is_unsigned_number(tmp))
-        {
-            print_invalid_arg_error(argv[arg_index]);
-            return ARG_BAD;
-        }
-
-        *out = atoi(tmp);        
-        return ARG_OK;
-    }
-
-    return ARG_NOT_FOUND;
-}
-
-UI get_serial_port_arg(int argc, char **argv, char **out)
-{
-    int arg_index = get_arg_index(
-        argc,
-        argv,
-        SERIAL_PORT_ARG_SHORT,
-        SERIAL_PORT_ARG_LONG
-    );
-
-    if(arg_index > -1)
-    {
-        /* if next arg can't be accessed return bad */
-        if(arg_index + 1 > argc - 1)
-        {
-            print_invalid_arg_error(argv[arg_index]);
-            return ARG_BAD;
-        }
-        
-        UI len = strlen(argv[arg_index + 1]);
-        free(*out);
-        *out = malloc(sizeof(char) * (len + 1));
-        strcpy(*out, argv[arg_index + 1]);
-
-        return ARG_OK;
-    }
-
-    return ARG_NOT_FOUND;
+    return ARG_BAD;
 }
--- daemon/main.c
+++ daemon/main.c
@@ -7,63 +7,84 @@
 #include<string.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<stdbool.h>
+
+#define ARGS_CNT                  5
+
+#define HELP_ARG                  "help"
+#define HELP_ARG_SHORT            "-h"
+#define HELP_ARG_LONG             "--help"
+
+#define UPDATE_INTERVAL_ARG       "update_interval"
+#define UPDATE_INTERVAL_ARG_SHORT "-u"
+#define UPDATE_INTERVAL_ARG_LONG  "--update-interval"
+
+#define INIT_TIMEOUT_ARG          "init_timeout"
+#define INIT_TIMEOUT_ARG_SHORT    "-i"
+#define INIT_TIMEOUT_ARG_LONG     "--init-timeout"
+
+#define INIT_TRY_SLEEP_ARG        "init_try_sleep"
+#define INIT_TRY_SLEEP_ARG_SHORT  "-I"
+#define INIT_TRY_SLEEP_ARG_LONG   "--init-try-sleep"
+
+#define SERIAL_PORT_ARG           "serial_port"
+#define SERIAL_PORT_ARG_SHORT     "-s"
+#define SERIAL_PORT_ARG_LONG      "--serial-port"
 
 #define DEFAULT_UPDATE_INTERVAL_MS 1000
-#define DEFAULT_INIT_TIMEOUT_MS 10000
-#define DEFAULT_INIT_TRY_SLEEP_MS 1500
-#define DEFAULT_SERIAL_PORT "/dev/ttyUSB0"
+#define DEFAULT_INIT_TIMEOUT_MS    10000
+#define DEFAULT_INIT_TRY_SLEEP_MS  1500
+#define DEFAULT_SERIAL_PORT        "/dev/ttyUSB0"
 
 int main(int argc, char **argv)
 {
-	if(check_for_help_arg(argc, argv) == ARG_OK) return 0;
+	Args args;
+	args_init(&args);
+
+	args_add(HELP_ARG, HELP_ARG_SHORT, HELP_ARG_LONG, true, &args);
+	args_add(UPDATE_INTERVAL_ARG, UPDATE_INTERVAL_ARG_SHORT, UPDATE_INTERVAL_ARG_LONG, false, &args);
+	args_add(INIT_TIMEOUT_ARG, INIT_TIMEOUT_ARG_SHORT, INIT_TIMEOUT_ARG_LONG, false, &args);
+	args_add(INIT_TRY_SLEEP_ARG, INIT_TRY_SLEEP_ARG_SHORT, INIT_TRY_SLEEP_ARG_LONG, false, &args);
+	args_add(SERIAL_PORT_ARG, SERIAL_PORT_ARG_SHORT, SERIAL_PORT_ARG_LONG, false, &args);
+
+	args_parse(argc, argv, &args);
 
 	/* init all of them to their default values */
 	UI update_interval_ms = DEFAULT_UPDATE_INTERVAL_MS;
 	UI init_timeout_ms = DEFAULT_INIT_TIMEOUT_MS;
 	UI init_try_sleep_ms = DEFAULT_INIT_TRY_SLEEP_MS;
-
 	char *serial_port = 
 	malloc(sizeof(char) * (strlen(DEFAULT_SERIAL_PORT) + 1));
 	strcpy(serial_port, DEFAULT_SERIAL_PORT);
 
-	if(get_update_interval_arg(
-		argc, 
-		argv, 
-		&update_interval_ms
-	) == ARG_BAD) return BAD_ARG_FOUND;
-
-	if(get_init_timeout_arg(
-		argc, 
-		argv, 
-		&init_timeout_ms
-	) == ARG_BAD) return BAD_ARG_FOUND;
-
-	if(get_init_try_sleep_arg(
-		argc, 
-		argv, 
-		&init_try_sleep_ms
-	) == ARG_BAD) return BAD_ARG_FOUND;
-
-	if(get_serial_port_arg(
-		argc,
-		argv,
-		&serial_port
-	) == ARG_BAD) return BAD_ARG_FOUND;
-
-	if(init_timeout_ms < init_try_sleep_ms)
+	if(args_get_bool(HELP_ARG, &args))
 	{
-		printf(
-			"Fatal error: init_timeout [%d] is less than init_try_sleep [%d]\n", 
-			init_timeout_ms, 
-			init_try_sleep_ms
-		);
+		print_help_message();
+		return EXIT_OK;
+	}
+
+	args_out_uint(UPDATE_INTERVAL_ARG, &update_interval_ms, &args);
+	args_out_uint(INIT_TIMEOUT_ARG, &init_timeout_ms, &args);
+	args_out_uint(INIT_TRY_SLEEP_ARG, &init_try_sleep_ms, &args);
+	args_out_str(SERIAL_PORT_ARG, &serial_port, &args);
+
+	if(init_try_sleep_ms > init_timeout_ms)
+	{
+		printf("Fatal error: init-try-sleep is bigger than init-timeout\n");
 		return BAD_ARG_FOUND;
 	}
+
+	printf("%s\n", serial_port);
 	
-	return start_memduino(
+	int status = start_memduino(
 		update_interval_ms,
 		init_timeout_ms,
 		init_try_sleep_ms,
 		serial_port
 	);
+
+	args_free(&args);
+	free(serial_port);
+
+	return status;
 }
