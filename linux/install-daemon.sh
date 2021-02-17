55693505c8a82f7cadd673bd5aa29206fa840579
oda404 <xdxalexandru404@gmail.com>
2021-02-17 16:44:53 +0200

    Add .argconfig file in /etc/memduino

--- linux/install-daemon.sh
+++ linux/install-daemon.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 #this script should only be executed through make install
+if ! [ -d "/etc/memduino" ]; then
+    sudo mkdir /etc/memduino
+    sudo cp $(dirname $BASH_SOURCE)/default-argconfig /etc/memduino/.argconfig
+elif ! [ -f "/etc/memduino/.argconfig" ]; then 
+    sudo cp $(dirname $BASH_SOURCE)/default-argconfig /etc/memduino/.argconfig
+fi
 sudo systemctl stop memduinod
 sudo cp $(dirname $BASH_SOURCE)/../build/daemon/memduinod /usr/bin/
 sudo cp $(dirname $BASH_SOURCE)/memduinod.service /usr/lib/systemd/system/
--- linux/uninstall-daemon.sh
+++ linux/uninstall-daemon.sh
@@ -1,3 +1,4 @@
 sudo systemctl stop memduinod
 sudo systemctl disable memduinod
 sudo rm /usr/lib/systemd/system/memduinod.service /usr/bin/memduinod
+sudo rm -rf /etc/memduino
